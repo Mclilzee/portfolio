54a5dd85426b7851b86f2ebb60395aa44703fe49
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-29 05:57:55 +0100

    Bring nav bar to life

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+const navToggle = document.querySelector("header .nav-toggle");
+if (navToggle == undefined) {
+  throw Error("Could not find nav toggle element");
+}
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
