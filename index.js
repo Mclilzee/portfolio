99da139e2cf33fabab08473295ba276de2fa0dbd
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-01-29 06:03:36 +0100

    Add functionality to hide nav when clicking links

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ if (navToggle == undefined) {
   throw Error("Could not find nav toggle element");
 }
 
+const navList = document.querySelector("header > nav > ul");
+if (navList == undefined) {
+  throw Error("Couldn't find nav links");
+}
+
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
+});
+
+navList.addEventListener("click", (e) => {
+  if (e.target.href == undefined) {
+    return;
+  }
+
+  document.body.classList.remove("nav-open");
 });
